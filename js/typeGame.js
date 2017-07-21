7b0f017cb3b0d272958a347ad67ad38cb5539dbf
bohdanh <bohdanhu@softwareplanet.uk.com>
2017-07-21 15:48:42 +0300

    Game type

---
+++ js/typeGame.js
@@ -0,0 +1,21 @@
+var buildButton = document.getElementById("game-build");
+
+
+function buildGameForFirebase() {
+    var numberOfRoundsElem = document.getElementById('number-tour');
+    var numberOfRounds = numberOfRoundsElem.value;
+    var gameBuilder = new GameBuilder();
+
+    for (let i = 0; i < numberOfRounds; i++) {
+        let numberOfQuizzesElement = document.getElementById('number-questions-' + (i + 1));
+
+        gameBuilder.addRound(i + 1, numberOfQuizzesElement.value);
+    }
+
+    return gameBuilder.buildGame().convertForFirebase();
+}
+function buildListener() {
+    let game = buildGameForFirebase();
+    let gameService = new GameService(DbConnection.getConnection());
+    gameService.save(game);
+}
