f418515bb94595675e06d70e6b45b7b874cb6c6d
bohdanh <bohdanhu@softwareplanet.uk.com>
2017-07-21 13:55:19 +0300

    GameBuider

--- js/Game.js
+++ js/Game.js
@@ -1,12 +1,32 @@
 class Game{
     constructor(){
-        this.id = 0;
         this.teams = [];
         this.rounds = [];
         this.results = [];
         this.resultService = null;
         this.teamService = null;
         this.gameService = null;
+    }
+
+    convertForFirebase(){
+        var temp = {};
+        for(let i = 0; i < this.teams.length; i++){
+            temp[this.teams[i].id] = this.teams[i].name;
+        }
+        this.teams = temp;
+
+        var roundsTemp = {};
+        for(let i = 0; i < this.rounds.length; i++){
+            roundsTemp[this.rounds[i].id] = this.rounds[i].quantityOfQuestions;
+        }
+        this.rounds = roundsTemp;
+
+        return this;
+    }
+
+
+    convertFromFirebase(){
+
     }
 
 
