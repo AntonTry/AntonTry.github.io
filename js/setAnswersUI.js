9ed3d2643912e7c2cd66402a54d49f812342d0ca
OleksiiLy <oleksiily@softwareplanetgroup.com>
2017-07-25 13:04:38 +0300

    teamBuilder

--- js/setAnswersUI.js
+++ js/setAnswersUI.js
@@ -8,12 +8,12 @@ class SetAnswers{
         this.currentQuiz;
     }
 
-    setResultHeader(quizNumber){
+    setResultHeader(){
         let resultHeader = document.querySelector('#resultHeader');
         let questionField = resultHeader.querySelector('h1');
         let answerField = resultHeader.querySelector('p');
-        questionField.innerHTML = 'Question #' + quizNumber;
-        answerField.innerHTML = 'Answer: some answer ' + quizNumber;
+        questionField.innerHTML = 'Question #' + this.currentQuiz;
+        answerField.innerHTML = 'Answer: some answer ' + this.currentQuiz;
     }
 
     createTeamInput(){
@@ -39,24 +39,14 @@ class SetAnswers{
             this.game = game.val();
             this.currentRound = this.game.currentRound;
             this.currentQuiz = this.game.currentQuiz;
-            this.fillRoundSelector();
+            this.setResultHeader();
             this.createQuizButtons(this.game.rounds[this.currentRound]);
-        });
-    }
-
-    fillRoundSelector(){
-        let roundSelector = document.querySelector('.round-selector');
-        let selectorOption = roundSelector.querySelector('#roundOption');
-        this.game.rounds.forEach(() => {
-            roundSelector.appendChild(document.importNode(selectorOption.content, true));
-        })
-        roundSelector.querySelectorAll('option').forEach((option,index) => {
-            option.innerHTML += index + 1;
+            this.setButtonsColor();
         });
     }
 
     createQuizButtons(quizzesCount){
-        let quizzesPanel = document.querySelector('.navigation-panel');
+        let quizzesPanel = document.querySelector('#quizPanel');
         let button = quizzesPanel.querySelector('#quizButton');
         for (let i = 1; i <= quizzesCount; i++){
             quizzesPanel.appendChild(document.importNode(button.content,true));
@@ -65,10 +55,19 @@ class SetAnswers{
     }
 
     setButtonsColor(){
-        let quizzesPanel = document.querySelector('.navigation-panel');
+        let quizzesPanel = document.querySelector('#quizPanel');
         let buttons = quizzesPanel.querySelectorAll('button');
         buttons.forEach((button,index) =>{
-
+            if(index < this.currentQuiz -1){
+                button.className = "btn btn-success";
+            }
+            else if(index == this.currentQuiz - 1){
+                button.className = "btn btn-info";
+            }
+            else if(index > this.currentQuiz - 1){
+                button.className = "btn";
+            }
+            button.innerHTML = index +1;
         })
 
 
@@ -78,11 +77,10 @@ class SetAnswers{
 }
 function generatePage() {
     let databese = DbConnection.getConnection();
-    let resultBuilder = new SetAnswers(new ResultService(databese), new GameService(databese));
-    resultBuilder.setResultHeader(1);
+    let setAnswer = new SetAnswers(new ResultService(databese), new GameService(databese));
     JSON.parse(localStorage.getItem('teams')).forEach((teamName) => {
-        resultBuilder.createTeamInput();
+        setAnswer.createTeamInput();
     })
-    resultBuilder.setTeamsNames();
-    resultBuilder.setGame();
+    setAnswer.setTeamsNames();
+    setAnswer.setGame();
 }
